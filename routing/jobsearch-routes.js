947c3b7cf4027c6a2640908a5cb509795ab58e37
uniqueregina666 <uniqueregina666@aol.com>
2018-04-30 11:19:55 -0400

    changing api routes

--- routing/jobsearch-routes.js
+++ routing/jobsearch-routes.js
@@ -1,30 +1,3 @@
-// Dependencies
-var express = require("express");
-var mysql = require("mysql");
-
-// Create express app instance.
-var app = express();
-
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-
-// MySQL DB Connection Information (remember to change this with our specific credentials)
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "jobsearch_db"
-});
-
-// Initiate MySQL Connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
 
 // Routes
 app.get("/", function(req, res) {
