7522489f2bedfaaeab074d1bdfd69699a0b92a0c
uniqueregina666 <uniqueregina666@aol.com>
2018-04-30 13:13:51 -0400

    fixing job search, routes and seeds

--- routing/jobsearch-routes.js
+++ routing/jobsearch-routes.js
@@ -1,41 +1,29 @@
 
 // Routes
-app.get("/", function(req, res) {
 
-  // If the main route is hit, then we initiate a SQL query to grab all records.
-  // All of the resulting records are stored in the variable "result."
-  connection.query("SELECT * FROM jobs", function(err, result) {
+var db = require("../models");
 
-    // We then begin building out HTML elements for the page.
-    var html = "<h1> Job Results </h1>";
+module.exports = function (app) {
 
-    // Here we begin an unordered list.
-    html += "<ul>";
-
-    // We then use the retrieved records from the database to populate our HTML file.
-    for (var i = 0; i < result.length; i++) {
-      html += "<li><p> ID: " + result[i].id + "</p>";
-      html += "<p>user ID: " + result[i].userID + " </p>";
-      html += "<p>Job Title: " + result[i].jobtitle + " </p>";
-      html += "<p>Location: " + result[i].location + " </p>";
-      html += "<p>Company: " + result[i].company + " </p>";
-      html += "<p>Contact: " + result[i].contact + " </p>";
-      html += "<p>Email: " + result[i].email+ " </p>";
-      html += "<p>Phone: " + result[i].phone + " </p>";
-      html += "<p>Webpage: " + result[i].webpage + " </p>";
-      html += "<p>Date Posted: " + result[i].createdAt + " </p></li>";
-    }
-
-    // We close our unordered list.
-    html += "</ul>";
-
-    // Finally we send the user the HTML file we dynamically created.
-    res.send(html);
+      // GET route for getting all of the posts
+  app.get("/api/jobssearch", function(req, res) {
+    // Add sequelize code to find all posts, and return them to the user with res.json
+    db.Post.findAll({}).then(function(result){
+      res.json(result);
+    })
   });
-});
 
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+  // Get route for returning posts of a specific category
+  app.get("/api/jobssearch/category/:category", function(req, res) {
+    // Add sequelize code to find all posts where the category is equal to req.params.category,
+    // return the result to the user with res.json
+    db.Post.findAll({
+      where: {
+        jobtitle: req.params.jobtitle,
+        location: req.params.location
+      }
+    }).then(function(result){
+      res.json(result);
+    })
+  });
+}
