05ccc8a3536bc06da4c90ab4f85a1c5483cfcd7c
Zach Schneider <zschneid85@yahoo.com>
2018-05-01 12:36:01 -0400

    adding friends.js

---
+++ public/js/friends.js
@@ -0,0 +1,68 @@
+// Data structure that will store the friends data
+var friendsArray = [{
+	name: 'Front-end Developer',
+	photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcScmszYCJRFKc9zhEUTjpk3DCG-Unk_qOpwyZAQrRd7fRvpnbqzww',
+	scores: [
+		'5',
+		'5',
+		'5',
+		'1',
+		'1',
+		'1',
+		'1',
+		'4',
+		'5',
+		'5'
+	]
+},
+{
+	name: 'Back-end Developer',
+	photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6uL5N6XB04UUa-yvIKF_mbrj7b6CjD1mDbYLPUG8y_gLidYna',
+	scores: [
+		'1',
+		'1',
+		'1',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5'
+	]
+},
+{
+	name: 'Skills of Both a Back and Front end Developer!!!!',
+	photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&h=350',
+	scores: [
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5',
+		'5'
+	]
+},
+{
+	name: 'This might not be the thing for you. I mean a Software Deleloper.',
+	photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWqjM0pwLavq3Y7zQe4YKAA7b63qm9yd3I9fY84zaEN3XyANSXxg',
+	scores: [
+		'1',
+		'1',
+		'1',
+		'1',
+		'1',
+		'1',
+		'1',
+		'1',
+		'1',
+		'1'
+	]
+}
+];
+
+module.exports = friendsArray;
