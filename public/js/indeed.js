5b981f93ebee3824b23adb64b91e09557e2012a1
Regina Ramos <uniqueregina666@aol.com>
2018-05-01 20:49:04 -0400

    fixing indeed js

--- public/js/indeed.js
+++ public/js/indeed.js
@@ -31,10 +31,15 @@ function renderJobs(data) {
   
         var div = $("<div>");
   
-        div.append("<h2>" + data[i].title + "</h2>");
-        div.append("<p>Author: " + data[i].author + "</p>");
-        div.append("<p>Genre: " + data[i].genre + "</p>");
-        div.append("<p>Pages: " + data[i].pages + "</p>");;
+        div.append("<h2>Job Title:" + data[i].jobtitle + "</h2>");
+        div.append("<p>Location: " + data[i].location + "</p>");
+        div.append("<p>Company: " + data[i].company + "</p>");
+        div.append("<p>Contact: " + data[i].contact + "</p>");;
+        div.append("<p>Job Overview" + data[i].joboverview + "</p>");
+        div.append("<p>Salary: " + data[i].salary + "</p>");
+        div.append("<p>Bonuses: " + data[i].bonuses + "</p>");
+        div.append("<p>Created at: " + data[i].createdAt+ "</p>");;
+       
         $("#stats").append(div);
   
       }
